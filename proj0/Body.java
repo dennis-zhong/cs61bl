adbe37a48c100823916f9a74582a0e99da64b2c1
dennis zhong <main.dzhong@gmail.com>
2020-06-23 18:37:17 -0700

    corrected api checks

--- proj0/Body.java
+++ proj0/Body.java
@@ -1,12 +1,12 @@
 public class Body {
-  double xxPos;
-  double yyPos;
-  double xxVel;
-  double yyVel;
-  double mass;
-  String imgFileName;
+  public double xxPos;
+  public double yyPos;
+  public double xxVel;
+  public double yyVel;
+  public double mass;
+  public String imgFileName;
 
-  final static double gcons = 6.67e-11; //Gravitational constant
+  private final static double gcons = 6.67e-11; //Gravitational constant
 
 //basic constructor
   public Body(double xP, double yP, double xV, double yV, double m, String img){
